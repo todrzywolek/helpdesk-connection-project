62375d9f75d8762cb7ed313ad2bfa6ae599b18b8
Tomasz Odrzywolek <tomeko92@gmail.com>
2018-12-01 01:42:06 +0100

    add config file for logger change build in app to create fat jar file

--- src/main/java/pl/edu/agh/ics/to/App.java
+++ src/main/java/pl/edu/agh/ics/to/App.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
-    private static final Logger LOGGER = LogManager.getLogger(App.class);
+    private static final Logger LOGGER = LogManager.getLogger("App");
 
     public static void main(String[] args) {
 
